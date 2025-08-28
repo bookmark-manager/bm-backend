1ce3288ec653ba584691d4a5b0c72883dcbb714f
abdugaffor2004 <abdughaffar7104@gmail.com>
2025-08-28 10:59:12 +0300

    feat: configure db launching

---
+++ database/migrations/00001_init.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS idx_bookmarks_created_at;
+DROP INDEX IF EXISTS idx_bookmarks_url;
+
+DROP TABLE IF EXISTS bookmarks;
---
+++ database/migrations/00001_init.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bookmarks (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE INDEX idx_bookmarks_url ON bookmarks(url);
+
+
+CREATE INDEX idx_bookmarks_created_at ON bookmarks(created_at);
+    
